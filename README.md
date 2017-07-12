1b393fdf71696d0d0cbb6380ae83afecf9fdbafa
tanti100 <fihartanti94@gmail.com>
2017-07-12 19:57:29 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# uts
+# dataretrieval
+Tokenstem
---
+++ home.php
@@ -0,0 +1,8 @@
+<p><h3>Nama Kelompok</h3></p>
+<p><h2>Fihartanti</h2><br>
+14.01.55.0032</p>
+<p><h2>Wahyu Marlia</h2><br>
+14.01.55.0001</p>
+<p><h2>Adhi Prasetyo</h2><br>
+  14.01.55.0029
+</p>
